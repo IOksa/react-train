75851bb0e0c84a8c6570c7327fa19097a23f79f2
IOksa <117656767+IOksa@users.noreply.github.com>
2023-06-24 14:41:14 +0300

    update

--- src/components/NewsSearchForm.js
+++ src/components/NewsSearchForm.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-export default function NewsSearchForm({ onSubmit }) {
-  const [query, setQuery] = useState('');
-
-  const handleChange = e => setQuery(e.currentTarget.value);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(query);
-    setQuery('');
+class NewsSearchForm extends Component {
+  state ={
+    query: '',
+  }
+  
+  handleChange = e => {
+    this.setState({query: e.currentTarget.value});
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={query} onChange={handleChange} />
-      <button type="submit">Искать</button>
-    </form>
-  );
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({query: ''});
+  };
+
+    render(){
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.query} onChange={this.handleChange} />
+          <button type="submit">Искать</button>
+        </form>
+      );
+  }
 }
+
+
+export default NewsSearchForm;
